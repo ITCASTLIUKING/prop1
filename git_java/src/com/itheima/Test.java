6977bd629fd5daebc6b350c0a3bfca7f3fae039d
LIUKING <LIUKING@itcast>
2018-12-21 11:03:50 +0800

    第一次修改

--- git_java/src/com/itheima/Test.java
+++ git_java/src/com/itheima/Test.java
@@ -3,5 +3,7 @@ package com.itheima;
 public class Test {
     public static void main(String[] args) {
         System.out.println(12);
+        System.out.println(12);
+        System.out.println(12);
     }
 }
